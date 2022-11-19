9c74582c1d88c8301505626ade2d09a956ee8b80
Louis Quinnell <_@ziorobo.co>
2022-11-19 17:03:10 +1100

    Render simple uncached annotations

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import "@logseq/libs"
 
+import { Annotation } from "./decoders.js"
+
 const plugin = await import("../package.json", { assert: { type: "json" } })
   .then(m => m.default)
 
@@ -10,6 +12,46 @@ function main() {
       logseq.UI.showMsg(`Hypotheseq v${plugin.version}`)
     },
   )
+
+  logseq.Editor.registerSlashCommand("Link annotation", async () => {
+    window.focus()
+    const link = await window.navigator.clipboard.readText()
+    await logseq.Editor.insertAtEditingCursor(
+      `{{renderer ${link}}}`,
+    )
+  })
+
+  logseq.App.onMacroRendererSlotted(async ({ payload, slot }) => {
+    const [link] = payload.arguments
+    const id = /^https:\/\/hyp\.is\/(.+?)\//.exec(link)?.[1]
+    if (id) {
+      const url = `https://api.hypothes.is/api/annotations/${id}`
+      const headers = new Headers({
+        Authorization: `Bearer ${HYPOTHESIS_TOKEN}`,
+      })
+
+      const data = await fetch(url, { headers })
+        .then(result => result.json())
+        .then(Annotation.parse)
+
+      const quote = data.target[0].selector.find(s =>
+        s.type === "TextQuoteSelector"
+      )
+
+      if (quote?.type !== "TextQuoteSelector") {
+        throw new Error("No TextQuoteSelector found")
+      }
+
+      return logseq.provideUI({
+        key: `hypothesis-${id}`,
+        slot,
+        template:
+          `<blockquote style="border-left-color:#BD1C2B"><div class="is-paragraph><span style="user-select:all">${quote.exact.trim()}</span><cite style="font-size:0.75em;white-space:nowrap;float:right;padding-top:0.5em">— <a href=${link}>${
+            decodeURI(data.uri)
+          }</a></cite></div></blockquote>`,
+      })
+    }
+  })
 }
 
 logseq.ready(main).catch(console.error)
